bd9832bcb544e73d1969d9ab8290c75ddd59fa97
briankip <kipropbrian@gmail.com>
2017-08-30 12:40:25 +0300

    init commit, changing presets

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace NotificationChannels\:channel_namespace\Exceptions;
+namespace NotificationChannels\AfricasTalking\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
